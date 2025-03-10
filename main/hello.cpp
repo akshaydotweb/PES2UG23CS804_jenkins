ef63331bd77e3f9653ee994a693bf9b668daebc3
akshaydotweb <pichaisundar318@gmail.com>
2025-03-10 15:42:57 +0530

    new commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,5 +1,10 @@
-#include <iostream> 
-int main() {
-	std::cout << "Hello" << std:
-	return 0;
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
 }
